0e23adad2d1262d7cd21f2de9df39b9c630146af
JhaSwechha <swechha.jha@atlan.com>
2024-03-08 17:43:14 +0530

    [ADD] more logs to debug

--- integration/admin-client/src/main/java/org/keycloak/admin/client/token/TokenManager.java
+++ integration/admin-client/src/main/java/org/keycloak/admin/client/token/TokenManager.java
@@ -18,6 +18,8 @@
 package org.keycloak.admin.client.token;
 
 import javax.ws.rs.client.WebTarget;
+
+import org.jboss.logging.Logger;
 import org.jboss.resteasy.client.jaxrs.ResteasyClient;
 import org.jboss.resteasy.client.jaxrs.ResteasyWebTarget;
 import org.keycloak.admin.client.Config;
@@ -43,7 +45,7 @@ import static org.keycloak.OAuth2Constants.USERNAME;
  */
 public class TokenManager {
     private static final long DEFAULT_MIN_VALIDITY = 30;
-
+    private static final Logger logger = Logger.getLogger(TokenManager.class);
     private AccessTokenResponse currentToken;
     private long expirationTime;
     private long refreshExpirationTime;
@@ -80,6 +82,7 @@ public class TokenManager {
     }
 
     public AccessTokenResponse grantToken() {
+        logger.error("Inside grant token change in token manager");
         Form form = new Form().param(GRANT_TYPE, accessTokenGrantType);
         if (PASSWORD.equals(accessTokenGrantType)) {
             form.param(USERNAME, config.getUsername())
